6a2a19f09a451189be1443109bf94c396ce064cd
Eliezer Croitoru <eliezer@ngtech.co.il>
2017-07-13 00:08:11 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM debian:stretch
+
+#ADD	sources.list	/etc/apt/sources.list
+
+RUN	apt-get update \
+	&& apt-get install -y apt-utils \ 
+	&& apt-get upgrade -y \
+	&& apt-get install -y linux-image-amd64 linux-headers-amd64 linux-source\
+	&& apt-get install -y build-essential iptables-dev iptables conntrack libnetfilter-conntrack-dev libxtables-dev \
+	&& apt-get install -y autoconf libtool git libpcap-dev libjson-c-dev \
+	&& apt-get clean -y && apt-get autoclean -y && apt-get autoremove -y
+
+ADD	clean.sh /clean.sh
+
+RUN	chmod +x /clean.sh \
+	&& /bin/bash clean.sh
+
+RUN mkdir /build
+VOLUME /build
+CMD ["/build/build.sh"]
+
---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -e 
+set -x 
+
+cd /build
+rm -rf ./nDPI
+#git clone -b netfilter https://github.com/vel21ripn/nDPI.git
+
+if [ -e "./vel21ripn-ndpi.tar" ]; then
+        tar xvf vel21ripn-ndpi.tar
+else
+        git clone https://github.com/vel21ripn/nDPI.git
+	tar cvf vel21ripn-ndpi.tar nDPI
+fi
+
+cd nDPI
+./autogen.sh
+# The autogen should run configure automatically
+#./configure
+declare -x PKG_CONFIG=`which pkg-config`
+KERNEL_VERSION=`ls /lib/modules/`
+# Patch files
+#a/ndpi-netfilter/ipt/Makefile
+# pkg-config xtables --cflags 
+ls /build/
+mkdir /build/destdir
+chmod 777 /build/destdir
+#declare -x  DESTDIR=/build/destdir/
+ls
+sed -i -e 's/-DOPENDPI_NETFILTER_MODULE/$(shell pkg-config --cflags xtables)/g' ndpi-netfilter/ipt/Makefile && \
+sed -i -e 's@MODULES_DIR := /lib/modules/$(shell uname -r)@MODULES_DIR := /lib/modules/$(shell ls /lib/modules/)@g' ndpi-netfilter/src/Makefile && \
+sed -i -e 's/depmod -a/depmod -a $(shell ls \/lib\/modules\/)/g' ndpi-netfilter/src/Makefile && \
+cd ndpi-netfilter/ && \
+make -j9 && \
+make install && \
+make modules_install && \
+echo $?
+mkdir -p /build/destdir/usr/lib/x86_64-linux-gnu/xtables && \
+cp /usr/lib/x86_64-linux-gnu/xtables/libxt_NDPI.so /build/destdir/usr/lib/x86_64-linux-gnu/xtables/ && \
+mkdir -p /build/destdir/usr/lib/modules/$KERNEL_VERSION/extra/ && \
+cp /build/nDPI/ndpi-netfilter/src/xt_ndpi.ko /build/destdir/usr/lib/modules/$KERNEL_VERSION/extra/xt_ndpi.ko && \
+cp /build/nDPI/ndpi-netfilter/src/xt_ndpi.ko /build/destdir/usr/lib/modules/$KERNEL_VERSION/extra/xt_ndpi.ko-non-stripped && \
+cd /build/destdir/usr/lib/x86_64-linux-gnu/xtables/ && \
+ln -s libxt_NDPI.so libxt_ndpi.so && \
+echo $?
+
+strip --strip-debug /build/destdir/usr/lib/modules/$KERNEL_VERSION/extra/xt_ndpi.ko
+
+set +x
+set +e
+#modprobe xt_ndpi && lsmod|grep ndpi
+#patch -p0 < /build/ipt-makefile.patch && \
+#patch -p0 < /build/src-makefile.patch && \
+
